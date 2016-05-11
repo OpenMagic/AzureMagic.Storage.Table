e7eee340d9091630746b206b4667a5e1ff20a42d
Tim Murphy <tim@26tp.com>
2016-05-11 19:34:14 +1000

    Disable parallel tests

--- tests/OpenMagic.Azure.Storage.Table.Specifications/Properties/AssemblyInfo.cs
+++ tests/OpenMagic.Azure.Storage.Table.Specifications/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
 ﻿using System.Reflection;
+using Xunit;
 
 [assembly: AssemblyTitle("OpenMagic.Azure.Storage.Table.Specifications")]
 [assembly: AssemblyDescription("Specifications for OpenMagic.Azure.Storage.Table project.")]
+
+// todo: enable parallel tests
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
