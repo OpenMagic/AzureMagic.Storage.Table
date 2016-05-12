b992182d3fa3cdd02319b973f257dcd9d39caff6
Tim Murphy <tim@26tp.com>
2016-05-13 04:23:55 +1000

    Add methods CreateQuery, ExecuteQueryAsync & GetCloudTable to ITable<TEntity>

--- source/OpenMagic.Azure.Storage.Table/ITable.cs
+++ source/OpenMagic.Azure.Storage.Table/ITable.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace OpenMagic.Azure.Storage.Table
 {
+    // todo: document
     public interface ITable<TEntity>
     {
+        TableQuery<DynamicTableEntity> CreateQuery();
+        Task<IEnumerable<TEntity>> ExecuteQueryAsync(TableQuery<DynamicTableEntity> query);
         Task<IEnumerable<TEntity>> FindByPartitionKeyAsync(string partitionKey);
         Task<IEnumerable<TEntity>> GetAllAsync();
+        CloudTable GetCloudTable();
     }
 }
--- source/OpenMagic.Azure.Storage.Table/Table.cs
+++ source/OpenMagic.Azure.Storage.Table/Table.cs
@@ -20,28 +20,12 @@ namespace OpenMagic.Azure.Storage.Table
             _serializer = serializer;
         }
 
-        public Task<IEnumerable<TEntity>> FindByPartitionKeyAsync(string partitionKey)
+        public TableQuery<DynamicTableEntity> CreateQuery()
         {
-            // partitionKey is not logged as it could contain sensitive information
-            LogTo.Trace($"{nameof(FindByPartitionKeyAsync)}.(partitionKey: XXXXX)");
-
-            var query = CreateQuery().Where(row => row.PartitionKey == partitionKey);
-
-            return ExecuteQueryAsync((TableQuery<DynamicTableEntity>)query);
+            return GetCloudTable().CreateQuery<DynamicTableEntity>();
         }
 
-        public Task<IEnumerable<TEntity>> GetAllAsync()
-        {
-            LogTo.Trace($"{nameof(GetAllAsync)}.()");
-            return ExecuteQueryAsync(CreateQuery());
-        }
-
-        private TableQuery<DynamicTableEntity> CreateQuery()
-        {
-            return GetTable().CreateQuery<DynamicTableEntity>();
-        }
-
-        private async Task<IEnumerable<TEntity>> ExecuteQueryAsync(TableQuery<DynamicTableEntity> query)
+        public async Task<IEnumerable<TEntity>> ExecuteQueryAsync(TableQuery<DynamicTableEntity> query)
         {
             const string logPrefix = nameof(ExecuteQueryAsync);
 
@@ -62,7 +46,23 @@ namespace OpenMagic.Azure.Storage.Table
             return rows.Select(row => _serializer.Deserialize(row));
         }
 
-        private CloudTable GetTable()
+        public Task<IEnumerable<TEntity>> FindByPartitionKeyAsync(string partitionKey)
+        {
+            // partitionKey is not logged as it could contain sensitive information
+            LogTo.Trace($"{nameof(FindByPartitionKeyAsync)}.(partitionKey: XXXXX)");
+
+            var query = CreateQuery().Where(row => row.PartitionKey == partitionKey);
+
+            return ExecuteQueryAsync((TableQuery<DynamicTableEntity>)query);
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            LogTo.Trace($"{nameof(GetAllAsync)}.()");
+            return ExecuteQueryAsync(CreateQuery());
+        }
+
+        public CloudTable GetCloudTable()
         {
             var storageAccount = CloudStorageAccount.Parse(_connectionString);
             var tableClient = storageAccount.CreateCloudTableClient();
